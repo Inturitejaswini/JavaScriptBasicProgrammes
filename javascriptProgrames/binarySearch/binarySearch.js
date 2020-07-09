972dad8d2dbeb50fe693faea975bbc02105e660c
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-09 19:55:17 +0530

    done with couponNumber programme

--- javascriptProgrames/binarySearch/binarySearch.js
+++ javascriptProgrames/binarySearch/binarySearch.js
@@ -1,14 +1,14 @@
-const readline = require('readline-sync');
-const util = require('./binarySearchBL');
-let fs = require('fs');
-try{ 
-const contents = fs.readFileSync('teja.txt', 'utf8');
-let replacedContents = contents.replace(',', ' ');
-let splitString = replacedContents.split(' ');
-splitString.sort();
-let input = readline.question('Enter a word to search in a file : ')
-    let isDataPresent = util.binarySearch(splitString, input);
-    isDataPresent =(isDataPresent>-1) ? 'Word is present in a file' :'Word is not present in a file'
-}catch(err){
-    console(err)
+const readline = require("readline-sync");
+const util = require("./binarySearchBL");
+let fs = require("fs");
+try {
+  const contents = fs.readFileSync("teja.txt", "utf8");
+  let replacedContents = contents.replace(",", " ");
+  let splitString = replacedContents.split(" ");
+  splitString.sort();
+  let input = readline.question("Enter a word to search in a file : ");
+  let isDataPresent = util.binarySearch(splitString, input);
+  isDataPresent =isDataPresent > -1 ? "Word is present in a file" : "Word is not present in a file";
+} catch (err) {
+  console.log(err);
 }
--- javascriptProgrames/couponNumber/couponNumber.js
+++ javascriptProgrames/couponNumber/couponNumber.js
@@ -1,11 +1,14 @@
 const readline = require('readline-sync');
 const util = require('./couponNumberBL');
+// try{
 let couponNumber = readline.question('Enter coupon no : ');
 if (couponNumber === '') {
     console.log('Give valid input');
-
 } else {
     let arr = util.toFindCouponNumber(couponNumber)
     let set = new Set(arr);
     console.log('random distinct numbers in' + set.size + 1 + ' attempt'); 
 }
+// }catch(err){
+//     console.log
+// }
