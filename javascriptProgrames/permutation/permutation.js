5a39399f0267d2fa6a805ff0b6ecbd5f472dd891
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-10 11:04:16 +0530

    [tejaswini]added comments and condition for permutation programme

--- javascriptProgrames/permutation/permutation.js
+++ javascriptProgrames/permutation/permutation.js
@@ -1,3 +1,15 @@
+/**
+ * Execution   : default node    cmd>node.permutaion.js
+ * 
+ * purpose     : Check if the arrays returned by two string functions are equal.
+ * 
+ * @description : The static functions to return all permutations of a String using iterative method andRecursion method.
+ * @file        :harmonic.js
+ * @overview    :find the permutations for an given user input string.
+ * @author      :Tejaswini
+ * @version     :1.0
+ * @since       :10-07-2020
+ * */
 const readline = require("readline-sync");
 const util = require("./permutationBL");
 try {
--- javascriptProgrames/permutation/permutationBL.js
+++ javascriptProgrames/permutation/permutationBL.js
@@ -1,28 +1,33 @@
+/**
+ * @Auther :Tejaswini
+ * @param { User input } string
+ * @purpose : this function is use to find the permutations for an given user input string.
+ * @Since : 10-07-2020
+ */
 exports.findPermutation = (string) => {
-    let fact = findFact(string);
-    function findFact(string) {
-        let factNumber = string.length;
-        let fact = 1;
-        while (factNumber !== 0) {
-            fact = fact * factNumber;
-            factNumber--;
-        }
-        return fact;
+  let fact = findFact(string);
+  function findFact(string) {
+    let factNumber = string.length;
+    let fact = 1;
+    while (factNumber !== 0) {
+      fact = fact * factNumber;
+      factNumber--;
     }
-    iteration(fact, string);
-    function iteration(fact, string) {
-        let k = 0;
-        console.log(string[k]);
-        for (let i = 0; i < fact; i++) {
-            for (let j = 0; j < string.length; j++) {
-                let replacedString = string.replace(string[k], string[j]);
-                console.log(replacedString);
-            }
-            if (k === string.length) {
-                k = 0;
-            } else {
-                k++;
-            }
-        }
-    }
-}
+    return fact;
+  }
+  iteration(fact, string);
+  function iteration(fact, string) {
+    let k = 0;
+    console.log(string[k]);
+    fact.forEach((factorial) => {
+      console.log(factorial);
+      factorial.string.forEach((stringFact) => {
+        console.log(stringFact);
+        let replacedString = string.replace(string[k], string[j]);
+        console.log(replacedString);
+      });
+    });
+    let result = k === string.length ? (k = 0) : k++;
+    console.log(result);
+  }
+};
