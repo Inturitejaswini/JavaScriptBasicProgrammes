0e4f0b82d7f774d75d253f1ff8634b410ca439b8
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-10 10:39:46 +0530

    [tejaswini]added conition in gamblerprogramme

--- javascriptProgrames/anagram/anagramBL.js
+++ javascriptProgrames/anagram/anagramBL.js
@@ -20,7 +20,7 @@ exports.checkAnagram = (str1, str2) => {
           }
         });
       });
-     let result = count === str1.length ? "anagram" : "not an anagrame";
+     let result = (count === str1.length) ? "anagram" : "not an anagrame";
      console.log(result);
     }
   } catch (error) {
