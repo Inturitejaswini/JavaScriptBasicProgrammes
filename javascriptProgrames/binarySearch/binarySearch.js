85b44104407de2b789486dd68b4c961ee722f969
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-10 08:42:09 +0530

    [tejaswini]added comments in binarysearch programme

--- javascriptProgrames/binarySearch/binarySearch.js
+++ javascriptProgrames/binarySearch/binarySearch.js
@@ -1,3 +1,15 @@
+/**
+ * Execution   : default node    cmd>node.binarySearch.js
+ * 
+ * purpose     : To find a word from a file
+ *  
+ * @description :Read in a list of words from File. Then prompt the user to enter a word to search the list. The program reports if the search word is found in the list.
+ * @file        :binarySearch.js
+ * @overview    :To find a word given by user from a file
+ * @author      :Tejaswini
+ * @version     :1.0
+ * @since       :9-07-2020
+ * */
 const readline = require("readline-sync");
 const util = require("./binarySearchBL");
 let fs = require("fs");
--- javascriptProgrames/binarySearch/binarySearchBL.js
+++ javascriptProgrames/binarySearch/binarySearchBL.js
@@ -1,19 +1,22 @@
-/**
- * @module(Array,input)sortArray,
- * input - sorted array and user input. 
+/**@author: Tejaswini
+ * @purpose : This function is use to find the search iteam into the sorted array using Binary Search for String!!
+ * @Since : 9-07-2020
+ * @param {Sorted Array} sortArray
+ * @param {Search item} input
  */
 exports.binarySearch = (sortArray, input) => {
-    try {
-        let front = 0, end = sortArray.length - 1, mid = Math.floor((front + end) / 2);
-        console.log(sortArray);
-        while (front <= end && sortArray[mid] != input) {
-            result= (input < sortArray[mid]) ? end = mid - 1 : front = mid + 1;
-            mid = Math.floor((front + end) / 2);
-        }
-        console.log(mid);
-        return (sortArray[mid] != input) ? -1 : mid + 1;
-    } catch (error) {
-        return error;
+  try {
+    let front = 0,
+      end = sortArray.length - 1,
+      mid = Math.floor((front + end) / 2);
+    console.log(sortArray);
+    while (front <= end && sortArray[mid] != input) {
+      result = input < sortArray[mid] ? (end = mid - 1) : (front = mid + 1);
+      mid = Math.floor((front + end) / 2);
     }
-
-}
+    console.log(mid);
+    return sortArray[mid] != input ? -1 : mid + 1;
+  } catch (error) {
+    return error;
+  }
+};
