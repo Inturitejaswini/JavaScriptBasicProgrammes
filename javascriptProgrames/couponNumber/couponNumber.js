9ffa549a69f5fd1c7a498ad94f01176745677c41
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-10 08:29:33 +0530

    [tejaswini]added comments and condition in counponnumber programme

--- javascriptProgrames/couponNumber/couponNumber.js
+++ javascriptProgrames/couponNumber/couponNumber.js
@@ -1,14 +1,21 @@
-const readline = require('readline-sync');
-const util = require('./couponNumberBL');
-// try{
-let couponNumber = readline.question('Enter coupon no : ');
-if (couponNumber === '') {
-    console.log('Give valid input');
-} else {
-    let arr = util.toFindCouponNumber(couponNumber)
-    let set = new Set(arr);
-    console.log('random distinct numbers in' + set.size + 1 + ' attempt'); 
+/**
+ * Execution : 1. default node cmd> node.couponNumber.js
+ * @Purpose :To find coupon number.
+ * @description :Given N distinct Coupon Numbers, how many random numbers do you need to generate distinct
+ * coupon number? This program simulates this random process.
+ * @overview    :Generate n random numbers and find the user entered number
+ * @file : couponNumber.js
+ * @author : Tejaswini
+ * @version : 1.0.0
+ * @since : 9-07-2020
+ */
+const readline = require("readline-sync");
+const util = require("./couponNumberBL");
+try {
+  let couponNumber = readline.question("Enter coupon no : ");
+  let arr = util.toFindCouponNumber(couponNumber);
+  let set = new Set(arr);
+  console.log("random distinct numbers in" + set.size + 1 + " attempt");
+} catch (err) {
+  console.log;
 }
-// }catch(err){
-//     console.log
-// }
--- javascriptProgrames/couponNumber/couponNumberBL.js
+++ javascriptProgrames/couponNumber/couponNumberBL.js
@@ -1,6 +1,10 @@
- /**
-     * @module(number)couponNumber as user input
-     */
+ /** 
+  @purpose :To find coupon number.
+  @Auther :Tejaswini
+  @version:  1.0
+  @return 
+  @Since : 9-07-2020
+  **/
     exports.toFindCouponNumber = (couponNumber) => {
         try {
             let arr = new Array();
