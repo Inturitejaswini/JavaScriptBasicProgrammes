54b425789c194ca5b608e7c802f250638993d552
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-10 10:51:26 +0530

    [tejaswini]added file comment for  gambler programme

--- javascriptProgrames/harmonic/harmonic.js
+++ javascriptProgrames/harmonic/harmonic.js
@@ -1,3 +1,15 @@
+/**
+ * Execution   : default node    cmd>node.harmonic.js
+ * 
+ * purpose     : To find harmonic number
+ * 
+ * @description :Prints the Nth harmonic number: 1/1 + 1/2 + ... + 1/N
+ * @file        :harmonic.js
+ * @overview    :find the harmonic number for an given user input
+ * @author      :Tejaswini
+ * @version     :1.0
+ * @since       :10-07-2020
+ * */
 let readline = require("readline-sync");
 let util = require("./harmonicBL");
 try {
