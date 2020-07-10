f59d626e2cd3e3152c4f84e858f66c0da4641ad7
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-10 10:55:31 +0530

    [tejaswini]added file comment for  harmonicprogramme

--- javascriptProgrames/harmonic/harmonicBL.js
+++ javascriptProgrames/harmonic/harmonicBL.js
@@ -1,6 +1,9 @@
 /**
-     * @module(number)nthNumber as-user input
-     */
+ * @Auther :Tejaswini
+ * @param { User input } nthNum
+ * @purpose : this function is use to find the harmonic number for an given user input.
+ * @Since : 10-07-2020
+ */
     exports.harmonicNumber = (nthNum) => {
         try {
             let value = 1;
