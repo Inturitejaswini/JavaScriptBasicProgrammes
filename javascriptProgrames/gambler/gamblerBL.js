6049c46417f1c732c0c516c6d70fed98994cc300
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-10 10:28:49 +0530

    [tejaswini]added conition in gamblerprogramme

--- javascriptProgrames/gambler/gamblerBL.js
+++ javascriptProgrames/gambler/gamblerBL.js
@@ -2,21 +2,18 @@
  * @module(stake,goal,num)as perameters to find the gambler num.
  */
 exports.gambler = (stake, goal, num) => {
-    let [wins, bets] = [0, 0];
-    for (let i = 0; i < num; i++) {
-        let temp = stake;
-        while (temp > 0 && temp < goal) {
-            bets++;
-            if (Math.random() < 0.5) {
-                temp++;
-            }
-            else {
-                temp--;
-            }
-        }
-        if (temp == goal) {
-            wins++;
-        }
+  let [wins, bets] = [0, 0];
+  for(let i in num) {
+    console.log(num);
+    let temp = stake;
+    while (temp > 0 && temp < goal) {
+      bets++;
+      let result = Math.random() < 0.5 ? temp++ : temp--;
+      console.log(result);
     }
-    return wins;
-}
+    if (temp == goal) {
+      wins++;
+    }
+  };
+  return wins;
+};
