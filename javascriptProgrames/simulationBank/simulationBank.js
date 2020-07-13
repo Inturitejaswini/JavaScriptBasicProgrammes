e60683a64c49df5ffec3bd585eeb734b383533c0
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-13 10:02:18 +0530

    [tejaswini]added file comments

--- javascriptProgrames/simulationBank/simulationBank.js
+++ javascriptProgrames/simulationBank/simulationBank.js
@@ -1,3 +1,16 @@
+/**
+ * Execution   : default node    cmd>node.simulationBank.js
+ * 
+ * purpose     : withdraw or deposit amount
+ * 
+ * 
+ * @description :Create a Program which creates Banking Cash Counter where people come in to deposit Cash and withdraw Cash. Have an input panel to add people to Queue to either deposit or withdraw money and dequeue the people. Maintain the Cash Balance.
+ * @file        :simulationBank.js
+ * @overview    :using queue to perform those operation 
+ * @author      :Tejaswini
+ * @version     :1.0
+ * @since       :13-07-2020
+ * */
 const readline = require("readline-sync");
 const util = require("./simulationBankBL");
 try{
