51394d66db1b9e1f6585fa1b61477f487a186dff
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-13 11:02:34 +0530

    [tejaswini]added inventory dataBL to utility file

--- utility/utility.js
+++ utility/utility.js
@@ -208,3 +208,27 @@ exports.hashingFunction = (data) => {
   }
   console.log(hashTable.get(12));
 };
+
+/**
+ * @author: Tejaswini
+ * @purpose : This function is use to find total cost for the inventory
+ * @Since : 10-07-2020
+ */
+exports.inventory=()=>{
+  const fs=require('fs');
+  try{
+    let text=fs.readFileSync('./inventoryDataManagement.json').toString('utf-8');
+    let jsonText=JSON.parse(text);
+    console.log(jsonText)
+    for(let data in jsonText.Grocery){
+        for(let i=0;i<jsonText.Grocery[data].length;i++){
+            let name = jsonText.Grocery[data[i]].name;
+            let weight = jsonText.Grocery[data[i]].weigth;
+            let price = jsonText.Grocery[data[i]].pricePerKg;
+            console.log('Name is : ' + name + ' ; Weight is : ' + weight + ' ; Prices is : ' + price + ' ; Total price is : ' + price * weight);
+        }
+    }
+  }catch(err){
+      console.log(err);
+  }
+}
