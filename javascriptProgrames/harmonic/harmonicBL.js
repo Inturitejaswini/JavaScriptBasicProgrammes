3634089f1cd718418281f4954c57baa8985a6d00
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-07-13 08:31:11 +0530

    [tejaswini]added hashingFunction file

--- javascriptProgrames/harmonic/harmonicBL.js
+++ javascriptProgrames/harmonic/harmonicBL.js
@@ -4,17 +4,6 @@
  * @purpose : this function is use to find the harmonic number for an given user input.
  * @Since : 10-07-2020
  */
-// exports.harmonicNumber = (nthNum) => {
-//     try {
-//         let value = 1;
-//         for (let index = 2; index <= nthNum; index++) {
-//             value = value + 1 / index;
-//         }
-//         console.log(value);
-//     } catch (error) {
-//         return error;
-//     }
-// }
 exports.harmonicNumber = (nthNum) => {
   try {
     let sum = 0.0;
--- javascriptProgrames/hashingFunction/hashingFunctionBL.js
+++ javascriptProgrames/hashingFunction/hashingFunctionBL.js
@@ -1,25 +1,25 @@
-exports.hashingFunction=(data)=>{
-    class HashTable{
-        constructor(){
-            this.buckets=new Array(50);
-        }
-        hash(data){
-            return data %50;
-        }
-        set(key,value){
-            let index=this.hash(key);
-            this.buckets[index]=value;
-        }
-        get(key){
-            const index =this.hash(key)
-                return this.buckets[index];
-        }
+exports.hashingFunction = (data) => {
+  class HashTable {
+    constructor() {
+      this.buckets = new Array(21);
     }
-    let hashTable=new HashTable();
-    let i=0;
-    while(i<data.length){
-        hashTable.set(data[i],data[i]);
-        i++;
+    hash(data) {
+      return data % 21;
     }
-    console.log(hashTable.get(5));
-}
+    set(key, value) {
+      let index = this.hash(key);
+      this.buckets[index] = value;
+    }
+    get(key) {
+      const index = this.hash(key);
+      return this.buckets[index];
+    }
+  }
+  let hashTable = new HashTable();
+  let i = 0;
+  while (i < data.length) {
+    hashTable.set(data[i], data[i]);
+    i++;
+  }
+  console.log(hashTable.get(12));
+};
